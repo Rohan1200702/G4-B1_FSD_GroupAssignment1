bd710557358fad47a7d303a3d0815b4a764c7a66
Rohan Chakraborty <115868116+Rohan1200702@users.noreply.github.com>
2022-12-18 21:36:18 +0530

    Update DepartmentMain.java

--- src/com/gl/department/main/DepartmentMain.java
+++ src/com/gl/department/main/DepartmentMain.java
@@ -12,11 +12,15 @@ import com.gl.department.model.TechDepartment;
 
 public class DepartmentMain {
 	public static void main(String[] args) {
+		//creating objects for each class to display the details throgh method showDetails
 		AdminDepartment admin = new AdminDepartment();
 		HrDepartment hr = new HrDepartment();
 		TechDepartment tech = new TechDepartment();
+		//showDetails method from Admin Department
 		admin.showDetails();
+		//showDetails method from HR Department
 		hr.showDetails();
+		//showDetails method from Tech Department
 		tech.showDetails();
 	}
 }
